922f94653a38812d57ba373274d4c3e8771aae14
coffiasd <759291502@qq.com>
2022-07-24 22:26:08 +0800

    fix:1.sol

--- contracts/1.sol
+++ contracts/1.sol
@@ -5,6 +5,8 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract NervosBridge {
+    event Received(address, uint);
+
     mapping(address => uint256) balances;
     
     uint256 maxAmount = 1000 ;
@@ -12,10 +14,13 @@ contract NervosBridge {
     using SafeMath for uint256;
 
     //reveive token from other contracts
-    receive() payable external{} 
+    receive() payable external{
+        emit Received(msg.sender,msg.value);
+    } 
 
+    //withdraw token with limited amount
     function withDrawSomeToken(uint256 amount,address payable to) public {
-        require(balances[msg.sender]<=maxAmount-amount);
+        require(balances[msg.sender]<=maxAmount-amount,"reach max amount");
         to.transfer(amount);
     }
 
